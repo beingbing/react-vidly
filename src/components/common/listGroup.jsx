cdca27c9670042dd7615dadab7d42e1d4626568a
Walkover <walkover@Walkovers-MacBook-Pro.local>
2021-06-01 00:05:58 +0530

    pfs : 15- sorting- extracting moviesTable : finished

--- src/components/common/listGroup.jsx
+++ src/components/common/listGroup.jsx
@@ -1,13 +1,12 @@
 const ListGroup = (props) => {
-  const { items, textProperty, valueProperty, selectedItem, onItemSelect } =
-    props;
+  const { items, textProperty, selectedItem, onItemSelect } = props;
 
   return (
     <ul className="list-group">
       {items.map((item) => (
         <li
           onClick={() => onItemSelect(item)}
-          key={item[valueProperty]}
+          key={item[textProperty]}
           className={
             item === selectedItem ? "list-group-item active" : "list-group-item"
           }
