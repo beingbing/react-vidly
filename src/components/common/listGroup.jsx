88179653e25d4bcde426ef9ec40c18647cc90e66
Walkover <walkover@Walkovers-MacBook-Pro.local>
2021-05-31 19:26:16 +0530

    pfs : 10- filtering- displaying items : finished

--- src/components/common/listGroup.jsx
+++ src/components/common/listGroup.jsx
@@ -1,5 +1,15 @@
-const ListGroup = () => {
-  return <h1>hello from list group</h1>;
+const ListGroup = (props) => {
+  const { items, textProperty, valueProperty } = props;
+
+  return (
+    <ul className="list-group">
+      {items.map((item) => (
+        <li key={item[valueProperty]} className="list-group-item">
+          {item[textProperty]}
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default ListGroup;
