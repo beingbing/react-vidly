95f662be570eefed0277ede16af7267f4e283cf4
Walkover <walkover@Walkovers-MacBook-Pro.local>
2021-06-01 10:59:43 +0530

    routing : 16- adding routes : finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
-import Movies from './components/movies';
+import "./App.css";
+import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import { Redirect, Switch } from "react-router";
 
 function App() {
   return (
     <main className="container">
-      <Movies />
+      <Switch>
+        <Route path="/movies" component={Movies}></Route>
+        <Route path="/customers" component={Customers}></Route>
+        <Route path="/rentals" component={Rentals}></Route>
+        <Route path="/not-found" component={NotFound}></Route>
+        <Redirect from="/" exact to="/movies"></Redirect>
+        <Redirect to="/not-found"></Redirect>
+      </Switch>
     </main>
   );
 }
