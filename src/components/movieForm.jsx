06f4efc189ed610ba00c35261a43698f3771df98
Walkover <walkover@Walkovers-MacBook-Pro.local>
2021-06-01 11:25:22 +0530

    routing : 18- linking to the movieForm : finished

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,5 +1,15 @@
-const MovieForm = () => {
-  return <h1>Movie form</h1>;
+const MovieForm = ({ match, history }) => {
+  return (
+    <div>
+      <h1>Movie form - {match.params.id}</h1>
+      <button
+        className="btn btn-primary"
+        onClick={() => history.push("/movies")}
+      >
+        Save
+      </button>
+    </div>
+  );
 };
 
 export default MovieForm;
