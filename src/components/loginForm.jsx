98e4b8cb0e24715814941ebe227bd8c85dc3a96a
Walkover <walkover@Walkovers-MacBook-Pro.local>
2021-06-01 12:50:38 +0530

    forms : 4- refs : finished

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -3,9 +3,9 @@ import React, { Component } from "react";
 class LoginForm extends Component {
   username = React.createRef();
 
-  componentDidMount() {
-    this.username.current.focus();
-  }
+  // componentDidMount() {
+  //   this.username.current.focus();
+  // }
 
   handleSubmit = (e) => {
     e.preventDefault();
@@ -21,6 +21,7 @@ class LoginForm extends Component {
           <div className="form-group">
             <label htmlFor="username">Username</label>
             <input
+              autoFocus
               ref={this.username}
               type="username"
               className="form-control"
