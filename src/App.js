8c7493027123337df66a3aeddfc2d863f5b2f8cd
Walkover <walkover@Walkovers-MacBook-Pro.local>
2021-06-01 11:13:48 +0530

    routing : 17- adding the navbar : finished

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import "./App.css";
+import React from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
 import Movies from "./components/movies";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
-import { Redirect, Switch } from "react-router";
+import NavBar from "./components/common/navBar";
+import "./App.css";
 
 function App() {
   return (
-    <main className="container">
-      <Switch>
-        <Route path="/movies" component={Movies}></Route>
-        <Route path="/customers" component={Customers}></Route>
-        <Route path="/rentals" component={Rentals}></Route>
-        <Route path="/not-found" component={NotFound}></Route>
-        <Redirect from="/" exact to="/movies"></Redirect>
-        <Redirect to="/not-found"></Redirect>
-      </Switch>
-    </main>
+    <React.Fragment>
+      <NavBar></NavBar>
+      <main className="container">
+        <Switch>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Redirect from="/" exact to="/movies"></Redirect>
+          <Redirect to="/not-found"></Redirect>
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
