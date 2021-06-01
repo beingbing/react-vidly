4fb1d00fbcafc2cc583d294ddd9623c4f7cc3257
Walkover <walkover@Walkovers-MacBook-Pro.local>
2021-06-01 15:03:48 +0530

    forms : 5- controlled elements : finished

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
-  username = React.createRef();
-
-  // componentDidMount() {
-  //   this.username.current.focus();
-  // }
+  state = {
+    account: { username: "", password: "" },
+  };
 
   handleSubmit = (e) => {
     e.preventDefault();
-    const username = this.username.current.value;
     console.log("submitted");
+  };
+
+  handleChange = (e) => {
+    const account = { ...this.state.account };
+    account.username = e.currentTarget.value;
+    this.setState({ account });
   };
 
   render() {
@@ -21,8 +24,8 @@ class LoginForm extends Component {
           <div className="form-group">
             <label htmlFor="username">Username</label>
             <input
-              autoFocus
-              ref={this.username}
+              value={this.state.account.username}
+              onChange={this.handleChange}
               type="username"
               className="form-control"
             />
